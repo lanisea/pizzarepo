55e3f238e075aa94890c15ccd779952cba475d26
David <david@pop-os.localdomain>
2019-09-20 22:14:36 -0400

    more changes

--- PIZZA.sh
+++ PIZZA.sh
@@ -5,19 +5,20 @@ tput setaf 6
 tput setab 1
 tput bold
 cat LOGO.txt | randtype -l
+sleep 1s
 
 #---------To start your order
 tput sgr0
-tput setaf 7
+tput bold
+tput setaf 1
 read -p "Hello, welcome to Generation Pizza! Would you like to start your order? " answer1 
 if [ "$answer1" = "yes" ] || [ "$answer1" = "Yes" ]
 then 
 tput bold
 tput setaf 3
-printf "\nHere are the daily specials!\n"
-sleep 1s
-printf "\nChoose any 2 or more for 10.99 each!\n"
-printf "\nAll pizzas, 3 toppings for 7.99!\n" 
+printf "\nHere are the daily specials!\n" | randtype -t 0,10000
+printf "\nChoose any 2 or more for 10.99 each!\n" | randtype -t 0,10000 
+printf "\nAll pizzas, 3 toppings for 7.99!\n" | randtype -t 0,10000
 else 
    echo "See you again!"
 fi
