901dab6fae6cd272ad3a08394292aa5c5cc3575e
David <david@pop-os.localdomain>
2019-09-20 21:53:38 -0400

    added style

--- PIZZA.sh
+++ PIZZA.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 clear
-
+tput setaf 6
+tput setab 1
+tput bold
 cat LOGO.txt | randtype -l
 
 #---------To start your order
+tput sgr0
+tput setaf 7
 read -p "Hello, welcome to Generation Pizza! Would you like to start your order? " answer1 
 if [ "$answer1" = "yes" ] || [ "$answer1" = "Yes" ]
 then 
- printf "Here are the daily specials!\n\nChoose any 2 or more for 10.99 each!\n\nAll pizzas, 3 toppings for 7.99!\n\n" 
+tput bold
+tput setaf 3
+printf "\nHere are the daily specials!\n"
+sleep 1s
+printf "\nChoose any 2 or more for 10.99 each!\n"
+printf "\nAll pizzas, 3 toppings for 7.99!\n" 
 else 
-   echo "Thank you please come again!" 
-fi 
-#-------------Selecting # of Pizzas
-read -p "How many pizza's would you like to order? " numofPizzas
+   echo "See you again!"
+fi
+#------------Pizza Select
+
+
 
 
 
