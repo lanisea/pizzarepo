7176963a7d12372a36ea5f8a30f0b0e01fbdf315
david <cdavid719@hotmail.com>
2019-09-20 22:47:21 -0400

    spaced lines

--- PIZZA.sh
+++ PIZZA.sh
@@ -11,16 +11,17 @@ sleep 1s
 tput sgr0
 tput bold
 tput setaf 1
-read -p "Hello, welcome to Generation Pizza! Would you like to start your order? " answer1 
+read -p "Hello, welcome to Generation Pizza! Would you like to start your order? " answer1
+
 if [ "$answer1" = "yes" ] || [ "$answer1" = "Yes" ]
 then 
 tput bold
 tput setaf 3
-printf "\nHere are the daily specials!\n" | randtype -t 0,10000
-printf "\nChoose any 2 or more for 10.99 each!\n" | randtype -t 0,10000 
-printf "\nAll pizzas, 3 toppings for 7.99!\n" | randtype -t 0,10000
+	printf "\nHere are the daily specials!\n" | randtype -t 0,10000
+	printf "\nChoose any 2 or more for 10.99 each!\n" | randtype -t 0,10000 
+	printf "\nAll pizzas, 3 toppings for 7.99!\n\n" | randtype -t 0,10000
 else 
-printf "\n\nSee you again!"
+	printf "\n\nSee you again!"
 sleep 2s
 clear
 fi
