cec8fffc7ccf234001e3a042b607863c4c2386bb
David Castillo <davidcastillo@pop-os.localdomain>
2019-09-24 19:15:03 -0400

    yo

--- DPIZZA.sh
+++ DPIZZA.sh
@@ -10,10 +10,12 @@ sleep 1s
 tput sgr0
 
 #------ARRAYS
-topArray=("Pepperoni" "Ham" "Pineapples" "Red Peppers" "Green Peppers" "Olives" "Sausage" "Meatballs")
 pizzaArray=("Cheese" "Pepperoni" "Hawaiian" "Veggie" "Vegan" "MeatLovers")
 sizeArray=("Extra Large" "Large" "Medium" "Small")
 priceArray=('$15.99' '$13.99' '$10.99' '$8.99')
+tPizzaArray=(*)
+tSizeArray=()
+counter=0
 #---------Greeting
 tput setaf 2
 tput bold
@@ -34,11 +36,6 @@ do
 printf "$(($p +1)). ${pizzaArray[$p]}\n" | randtype -t 0,1000
 done
 
-printf '\nChoose your own toppings! $1 each!\n' | randtype -t 0,1000
-for s in ${!topArray[*]}
-do
-printf "$(($s +1)). ${topArray[$s]}\n" | randtype -t 0,1000
-done
 #=================================================Starting off
 tput setaf 3
 printf "\n\n===============================\n\n" | randtype -l
@@ -68,13 +65,14 @@ tput setaf 1
 
 printf "\n\n===============================\n\n"
 
-read -p "How many pizzas are you getting today? " n3
 
 #-------------Loop for amount of pizzas
+read -p "How many pizzas are you getting today? " n3
+
 for x in $(seq $n3)
 do
  printf "\nChoose your size:\n"
- PS3="Please select your size:  "
+PS3="Please select your size:  "
   select size in "${sizeArray[@]}" 
    do
 	case $size in
@@ -88,11 +86,12 @@ do
 		break;;
 		*)
 		printf "Sorry, what was that again?\n\n "
+        userSize[$x]=$PS3
        esac
 done
 printf "\nChoose your style:\n"
-PS3="Please select the type of pizza: "
-     select style  in "${pizzaArray[@]}"
+PS2="Please select the type of pizza:"
+     select style in "${pizzaArray[@]}"
          do
         case $style in
         "Cheese")
@@ -107,18 +106,21 @@ PS3="Please select the type of pizza: "
                 break;;
         "Meat Lovers")
                 break;;
-        *) printf "Sorry, what was that again?\n"
-          esac
+        	'*') printf "Sorry, what was that again?\n"
+     userStyle[$x]=$PS2          
+esac
+
 done
 done
+
 
 #==================SUMMARY
 tput setaf 7
 tput bold
 printf "==================\nOrder\n==================\n"
-for s in ${!sizeArray[*]}
+for s in ${!userSize[*]}
 do
-printf "$(($s+1)). ${style[$s]} - ${size[$s]}\n"
+printf "$(($s+1)). ${userSize[$s]} - ${userStyle[$s]}\n"
 done
 printf "=================\nHave a good day!\n=================\n"
 
