e15c65938c3395da53235d19627aa71ea724ff0c
david <cdavid719@hotmail.com>
2019-09-24 15:55:38 -0400

    yes

--- DPIZZA.sh
+++ DPIZZA.sh
@@ -17,27 +17,27 @@ priceArray=('$15.99' '$13.99' '$10.99' '$8.99')
 #---------Greeting
 tput setaf 2
 tput bold
-printf "\n\nWelcome to PIZZA GENERATION!\n" | randtype -t 0,100000
+printf "\n\nWelcome to PIZZA GENERATION!\n" | randtype -t 0,10000
 tput sgr0
-sleep 2s
+sleep 1s
 #------------------------------=======----------PIZZA MENU
 tput setaf 3
 tput bold
-printf "\nOur pizza sizes:\n" | randtype -t 0,10000
+printf "\nOur pizza sizes:\n" | randtype -t 0,1000
 for p in ${!sizeArray[*]}
 do
-printf "$(($p +1)). ${sizeArray[$p]}-${priceArray[$p]}\n" | randtype -l
+printf "$(($p +1)). ${sizeArray[$p]}-${priceArray[$p]}\n" | randtype -t 0,1000
 done
-printf "\nOur signature pizzas:\n" | randtype -t 0,10000
+printf "\nOur signature pizzas:\n" | randtype -t 0,1000
 for p in ${!pizzaArray[*]}
 do
-printf "$(($p +1)). ${pizzaArray[$p]}\n" | randtype -l
+printf "$(($p +1)). ${pizzaArray[$p]}\n" | randtype -t 0,1000
 done
 
-printf '\nChoose your own toppings! $1 each!\n' | randtype -t 0,10000
+printf '\nChoose your own toppings! $1 each!\n' | randtype -t 0,1000
 for s in ${!topArray[*]}
 do
-printf "$(($s +1)). ${topArray[$s]}\n" | randtype -l
+printf "$(($s +1)). ${topArray[$s]}\n" | randtype -t 0,1000
 done
 #=================================================Starting off
 tput setaf 3
@@ -49,7 +49,7 @@ if [ $n2 = "yes" ] || [ $n2 = "Yes" ] || [ $n2 = "Y" ] || [ $n2 = "y" ]
 then
 tput bold
 tput setaf 3 
-printf "\n\nLet's start!\n" | randtype -l
+printf "\n\nLet's start!\n" 
 sleep 1s
 else
 tput setaf 1
@@ -73,57 +73,52 @@ read -p "How many pizzas are you getting today? " n3
 #-------------Loop for amount of pizzas
 for x in $(seq $n3)
 do
- echo "Choose your size:"
+ printf "\nChoose your size:\n"
  PS3="Please select your size:  "
   select size in "${sizeArray[@]}" 
    do
 	case $size in
 	"Extra Large")
-		printf '\nExtra Large is $15.99\n'
 		break;;
 	"Large")
-		printf '\nLarge is $13.99\n'
 		break;;
 	"Medium")
-		printf '\nMedium is $10.99\n'
 		break;;
 	"Small")
-		printf '\nSmall is $8.99\n'
 		break;;
-	*) printf "Sorry, what was that again?\n "
+		*)
+		printf "Sorry, what was that again?\n\n "
        esac
-    done
-
-sleep 1
-
+done
+printf "\nChoose your style:\n"
+PS3="Please select the type of pizza: "
      select style  in "${pizzaArray[@]}"
          do
         case $style in
         "Cheese")
-                printf '\nCheese\n'
                 break;;
         "Pepperoni")
-                printf '\nPepperoni\n'
                 break;;
         "Hawaiian")
-                printf '\nHawaiian\n'
                 break;;
         "Veggie")
-                printf '\nVeggie\n'
                 break;;
         "Vegan")
-                printf '\nVegan\n'
                 break;;
         "Meat Lovers")
-                printf '\nMeat Lovers\n'
                 break;;
         *) printf "Sorry, what was that again?\n"
           esac
-        done
- 
-
-
-
-
-
 done
+done
+
+#==================SUMMARY
+tput setaf 7
+tput bold
+printf "==================\nOrder\n==================\n"
+for s in ${!sizeArray[*]}
+do
+printf "$(($s+1)). ${style[$s]} - ${size[$s]}\n"
+done
+printf "=================\nHave a good day!\n=================\n"
+
