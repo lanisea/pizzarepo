6e3b000abbf53ccd5c0a7545e202722fc6075b00
Kristy Tarano <kristytarano@gmail.com>
2019-09-24 14:44:50 -0400

    Added 1723$ and new question

--- DPIZZA.sh
+++ DPIZZA.sh
@@ -13,7 +13,7 @@ tput sgr0
 topArray=("Pepperoni" "Ham" "Pineapples" "Red Peppers" "Green Peppers" "Olives" "Sausage" "Meatballs")
 pizzaArray=("Cheese" "Pepperoni" "Hawaiian" "Veggie" "Vegan" "MeatLovers")
 sizeArray=("Extra Large" "Large" "Medium" "Small")
-
+priceArray=('$15.99' '$13.99' '$10.99' '$8.99')
 #---------Greeting
 tput setaf 2
 tput bold
@@ -23,12 +23,16 @@ sleep 2s
 #------------------------------=======----------PIZZA MENU
 tput setaf 3
 tput bold
+printf "\nOur pizza sizes:\n" | randtype -t 0,10000
+for p in ${!sizeArray[*]}
+do
+printf "$(($p +1)). ${sizeArray[$p]}-${priceArray[$p]}\n" | randtype -l
+done
 printf "\nOur signature pizzas:\n" | randtype -t 0,10000
 for p in ${!pizzaArray[*]}
 do
 printf "$(($p +1)). ${pizzaArray[$p]}\n" | randtype -l
 done
-
 
 printf '\nChoose your own toppings! $1 each!\n' | randtype -t 0,10000
 for s in ${!topArray[*]}
@@ -59,10 +63,20 @@ tput sgr0
 tput bold
 tput setaf 6
 #==========================SELECTION
-echo "Our size pizzas:"
-PS3="Please select your size:  "
-select size in "${sizeArray[@]}" 
+sleep 1
+tput setaf 1
+
+printf "\n\n===============================\n\n"
+
+read -p "How many pizzas are you getting today? " n3
+
+#-------------Loop for amount of pizzas
+for x in $(seq $n3)
 do
+ echo "Choose your size:"
+ PS3="Please select your size:  "
+  select size in "${sizeArray[@]}" 
+   do
 	case $size in
 	"Extra Large")
 		printf '\nExtra Large is $15.99\n'
@@ -77,6 +91,39 @@ do
 		printf '\nSmall is $8.99\n'
 		break;;
 	*) printf "Sorry, what was that again?\n "
-esac
-done
+       esac
+    done
 
+sleep 1
+
+     select style  in "${pizzaArray[@]}"
+         do
+        case $style in
+        "Cheese")
+                printf '\nCheese\n'
+                break;;
+        "Pepperoni")
+                printf '\nPepperoni\n'
+                break;;
+        "Hawaiian")
+                printf '\nHawaiian\n'
+                break;;
+        "Veggie")
+                printf '\nVeggie\n'
+                break;;
+        "Vegan")
+                printf '\nVegan\n'
+                break;;
+        "Meat Lovers")
+                printf '\nMeat Lovers\n'
+                break;;
+        *) printf "Sorry, what was that again?\n"
+          esac
+        done
+ 
+
+
+
+
+
+done
